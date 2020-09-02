f4e35c123561c8042cc30846329cc6867c04d0f1
Bankkittirat <banklovecom1669@gmail.com>
2020-09-02 11:34:49 +0700

    Updates

---
+++ precache-manifest.f11c8a47a8e7be3e3ee501edcd8f8fb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5d3650d50c149e93779aebfb6d721e",
+    "url": "/Xo1/index.html"
+  },
+  {
+    "revision": "c18af4fcdad31c6802ab",
+    "url": "/Xo1/static/css/main.2a34271c.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/Xo1/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Xo1/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c18af4fcdad31c6802ab",
+    "url": "/Xo1/static/js/main.f0d77099.chunk.js"
+  },
+  {
+    "revision": "585934cc078d5840a9fa",
+    "url": "/Xo1/static/js/runtime-main.a3519e9e.js"
+  }
+]);
